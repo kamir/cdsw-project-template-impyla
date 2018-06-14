da5cae42dc845072f9d58677bf7210b49afd04c7
Mirko Kämpf <mirko.kaempf@gmail.com>
2018-06-14 13:38:04 +0200

    Update access_impala_via_python.py

--- access_impala_via_python.py
+++ access_impala_via_python.py
@@ -1,13 +1,16 @@
-conn = connect(host='cdsw-demo-3.vpc.cloudera.com', 
+
+IMPALA_HOST = 'cdsw-demo-3.vpc.cloudera.com'
+
+conn = connect(host=IMPALA_HOST, 
                auth_mechanism='GSSAPI', 
                use_ssl=False, 
                database='default', 
                port=21050)  
 
 cur = conn.cursor()
+
 cur.execute('SHOW TABLES')
 
-# now we are ready to get all tablenames ...
 cur.fetchall()
 
 
