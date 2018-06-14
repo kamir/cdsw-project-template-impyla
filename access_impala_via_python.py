e5d65e0aed9e762efdc2e7729c56512eb3d3d461
Mirko Kämpf <mirko.kaempf@gmail.com>
2018-06-14 13:45:07 +0200

    Update access_impala_via_python.py

--- access_impala_via_python.py
+++ access_impala_via_python.py
@@ -1,16 +1,18 @@
 
-IMPALA_HOST = 'cdsw-demo-3.vpc.cloudera.com'
+from impala.dbapi import connect
+
+IMPALA_HOST='cdsw-demo-3.vpc.cloudera.com'
+IMPALA_PORT=21050
 
 conn = connect(host=IMPALA_HOST, 
                auth_mechanism='GSSAPI', 
                use_ssl=False, 
                database='default', 
-               port=21050)  
+               port=IMPALA_PORT)  
 
 cur = conn.cursor()
 
 cur.execute('SHOW TABLES')
 
 cur.fetchall()
-
 
